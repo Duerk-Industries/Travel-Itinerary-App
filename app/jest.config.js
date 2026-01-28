15f711ff5d3ef90feef802b47facc37a6d7ef65f
Bryan Duerk <bryan.duerk@gmail.com>
2026-01-28 09:35:57 -0500

    Eliminated some test warnings

--- app/jest.config.js
+++ app/jest.config.js
@@ -10,10 +10,12 @@ module.exports = {
   moduleNameMapper: {
     '^react-native$': '<rootDir>/tests/__mocks__/react-native.ts',
     '^@react-native-async-storage/async-storage$': '<rootDir>/tests/__mocks__/asyncStorage.ts',
+    '^@react-native-community/datetimepicker$': '<rootDir>/tests/__mocks__/@react-native-community/datetimepicker.ts',
   },
   globals: {
     'ts-jest': {
       tsconfig: './tsconfig.json',
     },
   },
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
 };
